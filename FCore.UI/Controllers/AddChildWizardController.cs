ed86412da05c404ac1225528933446b9b6630423
almogaya1315 <liormatsliah1985@gmail.com>
2017-05-05 13:18:12 +0300

    add child page

---
+++ FCore.UI/Controllers/AddChildWizardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FCore.UI.Controllers
+{
+    public class AddChildWizardController : Controller
+    {
+        public ActionResult StartWizard()
+        {
+            return View();
+        }
+    }
+}
