8c54472092d91f593ff7031cb8cb36c807a56cdd
almogaya1315 <liormatsliah1985@gmail.com>
2017-05-07 09:11:11 +0300

    back to prev. wizard page

--- FCore.UI/App_Start/RouteConfig.cs
+++ FCore.UI/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace FCore.UI
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "FamilyCore", action = "Main", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "AddChildWizard",
+                url: "{controller}/{action}/{id}",
+                defaults: new { contoller = "AddChildWizard", action = "AddChild", id = UrlParameter.Optional }
+            );
         }
     }
 }
--- FCore.UI/Scripts/configcont.js
+++ FCore.UI/Scripts/configcont.js
@@ -7,3 +7,10 @@
 //    var path = $("#input_file").val();
 //    $("#file_sel_path").text = path;
 //});
+
+$(function () {
+    $('#back_from_pi').on('click', function () {
+        var url = '@Url.Action("LoadProfileImage", "AddChildWizard", Model)'
+        $('#wizard_page').load(url)
+    })
+})
