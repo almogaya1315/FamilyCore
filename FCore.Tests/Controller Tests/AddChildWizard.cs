14c4d6373c93d5ad080c7891cec0bd149bc7c77c
almogaya1315 <liormatsliah1985@gmail.com>
2017-05-16 16:47:07 +0300

    wizard unit test

---
+++ FCore.Tests/Controller Tests/AddChildWizard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FCore.Tests.Controller_Tests
+{
+    [TestClass]
+    public class AddChildWizard
+    {
+        [TestMethod]
+        public void ConnectRelatives() // signature to be fully detailed
+        {
+        }
+    }
+}
