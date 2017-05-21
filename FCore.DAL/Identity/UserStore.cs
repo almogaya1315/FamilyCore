3efbb69db17dbd92eaff2ed5efcdcbdd54401c94
almogaya1315 <liormatsliah1985@gmail.com>
2017-05-21 23:44:04 +0300

    misc.

---
+++ FCore.DAL/Identity/UserStore.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCore.DAL.Identity
+{
+    public class UserStore : IUserPasswordStore<User, int>
+    {
+        readonly UserContext context;
+
+        public UserStore(UserContext _context)
+        {
+            context = _context;
+        }
+
+        public Task CreateAsync(User user)
+        {
+            context.Users.Add(user);
+            return context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(User user)
+        {
+            context.Users.Remove(user);
+            return context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindByIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasPasswordAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(User user, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
