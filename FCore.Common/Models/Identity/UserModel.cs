7ed2465c8cfb56d6cc71a9a7fb32397b249f5206
almogaya1315 <liormatsliah1985@gmail.com>
2017-05-22 15:19:18 +0300

    user identity work flow

---
+++ FCore.Common/Models/Identity/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCore.Common.Models.Identity
+{
+    public class UserModel : IdentityUser
+    {
+    }
+}
