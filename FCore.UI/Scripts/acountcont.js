dda2a2597aa7f6d39c7f0bf421c6711f60e0a9ba
almogaya1315 <liormatsliah1985@gmail.com>
2017-06-05 11:27:36 +0300

    flow

---
+++ FCore.UI/Scripts/acountcont.js
@@ -0,0 +1,50 @@
+﻿// LoadFamiliesDynamic
+
+$(function () {
+    var relfam = $('#relfam_droptext');
+    relfam.on('input', function () {
+        var text = relfam.val();
+        if (text >= 2) {
+            $.ajax({
+                url: '/Acount/LoadFamiliesDynamic',
+                type: 'Get',
+                data: text,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function () {
+                    var relmem_text = $('#relmem_droptext');
+                    relmem_text.prop('disabled', false);
+                    var relmem = $('#memenum_box');
+                    relmem.prop('disabled', false);
+
+                    var url = '@Url.Action("LoadFamiliesDynamic", "Acount")';
+                    $('#famenum_div').load(url);
+                }
+            })
+        }
+    })
+})
+
+// LoadMembersDynamic
+
+$(function () {
+    var relmem = $('#relmem_droptext');
+    relmem.on('input', function () {
+        var text = relmem.val();
+        if (text >= 2) {
+            $.ajax({
+                url: '/Acount/LoadMembersDynamic',
+                type: 'Get',
+                data: text,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function () {
+                    var url = '@Url.Action("LoadMembersDynamic", "Acount")';
+                    $('#memenum_div').load(url);
+                }
+            })
+        }
+    })
+})
